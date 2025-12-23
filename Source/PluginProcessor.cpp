7cc8c1bb6ddb0cd3108b48296c8ac96747fbc093
Tianjian Ma <mma33@Illinois.edu>
2025-12-23 10:44:24 -0600

    Feat: Add state persistence for preset recall

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -251,15 +251,18 @@ juce::AudioProcessorEditor* ErodeAudioProcessor::createEditor()
 //==============================================================================
 void ErodeAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = apvts.copyState();
+    auto xml = state.createXml();
+    copyXmlToBinary(*xml, destData);
 }
 
 void ErodeAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    auto xml = getXmlFromBinary(data, sizeInBytes);
+	if (xml != nullptr && xml->hasTagName(apvts.state.getType())) {
+        auto state = juce::ValueTree::fromXml(*xml);
+        apvts.replaceState(state);
+    }
 }
 
 //==============================================================================
